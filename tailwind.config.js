376e3b20bfc2ffbf95ddf20682897d4682767e6c
Sevara <nematullayevasevara6@gamil.com>
2024-11-19 13:32:27 +0500

    lesson16 homework with only one page with responive

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Include the root-level HTML file
+    "./src/**/*.{html,js}", // Include other HTML and JS files inside src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
